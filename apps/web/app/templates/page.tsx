a9add344078cfb9acae0f632a41ed48c16ec525a
rsdiaz <robertoserrano83@gmail.com>
2025-04-28 01:09:51 +0200

    feat(frontend): add app dialog component

--- apps/web/app/templates/page.tsx
+++ apps/web/app/templates/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import templates from "../../templates/list.json"
+import { AppDialog } from "../components/app-dialog"
 
 export default function TemplatesPage() {
 
@@ -7,14 +8,18 @@ export default function TemplatesPage() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">Templates</h1>
+      <h1 className="text-3xl font-bold mb-8">Templates</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 gap-4">
         {
           templates.map((template) => (
-            <div key={template.slug} className="flex items-center justify-center py-4 px-2 gap-4 bg-background border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-              <Image src={`/templates/${template.name}/assets/${template.logo}`} alt={template.name} width="50" height="50" className="rounded-lg" />
-              <h2 className="font-bold">{template.name}</h2>
-            </div>
+            <AppDialog key={template.slug} template={template}>
+              <div className="flex flex-col items-center justify-between py-6 px-2 gap-4 bg-background border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+                <div>
+                  <Image src={`/templates/${template.name}/assets/${template.logo}`} alt={template.name} width="100" height="100" className="rounded" />
+                </div>
+                <h2 className="font-bold">{template.name}</h2>
+              </div>
+            </AppDialog>
           )) 
         }
       </div>
